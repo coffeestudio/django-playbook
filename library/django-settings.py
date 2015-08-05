0dd00fba03926fa68fa2464094a0a3504d1d916a
Art <art@shesha.ru>
2015-08-05 09:30:14 +0300

    fix django-settings module

--- library/django-settings.py
+++ library/django-settings.py
@@ -21,12 +21,23 @@ else:
 os.setuid(change_uid)
 
 settings = None
-settings_re = re.compile('\s*settings\s*=.*$')
 with open(manage_script) as f:
+    settings_start_re = re.compile('^\s*if\s+__name__\s+==\s+[\'"]__main__[\'"]\s*:\s*')
+    settings_end_re = re.compile('^\s*execute_from_command_line(sys.argv)\s*')
+    run_line = False
     for line in f:
-        if settings_re.match(line):
-            exec(line.strip())
+        print "LINE: %s" % line
+        if not run_line and settings_start_re.match(line):
+            run_line = True
+            continue
+        elif 'DJANGO_SETTINGS_MODULE' in os.environ and os.environ['DJANGO_SETTINGS_MODULE'] or settings_end_re.match(line):
             break
+        if run_line:
+            try:
+                exec(line.strip())
+            except:
+                pass
+    settings = os.environ.get('DJANGO_SETTINGS_MODULE', None)
 
 if not settings:
     raise RuntimeError
