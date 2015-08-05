b4b9532e7e6db9284cd3144d75bc858bc460910a
Art <art@shesha.ru>
2015-08-05 09:53:39 +0300

    ++

--- library/django-settings.py
+++ library/django-settings.py
@@ -26,17 +26,15 @@ with open(manage_script) as f:
     settings_end_re = re.compile('^\s*execute_from_command_line(sys.argv)\s*')
     run_line = False
     for line in f:
-        print "LINE: %s" % line
+        if not line:
+            continue
         if not run_line and settings_start_re.match(line):
             run_line = True
             continue
-        elif 'DJANGO_SETTINGS_MODULE' in os.environ and os.environ['DJANGO_SETTINGS_MODULE'] or settings_end_re.match(line):
+        elif 'DJANGO_SETTINGS_MODULE' in os.environ or settings_end_re.match(line):
             break
         if run_line:
-            try:
-                exec(line.strip())
-            except:
-                pass
+            exec(line.strip())
     settings = os.environ.get('DJANGO_SETTINGS_MODULE', None)
 
 if not settings:
